e22f815678c43a32b0b66c55a73edda9e21527bd
Bryan Chiu <bchiu@ca.ibm.com>
2018-04-03 20:40:16 -0700

    logic mostly present. determine structs and data passing

--- server/caching.go
+++ server/caching.go
@@ -2,82 +2,106 @@ package server
 
 import (
 	"fmt"
+
+	"proj2_f5w9a_h6v9a_q7w9a_r8u8_w1c0b/serverpb"
+
 	"github.com/dgraph-io/badger"
 	"math/rand"
 	"time"
 )
 
-const MAX_SIZE = 100
-const CACHE_SIZE = 10
+const (
+	MAX_BYTE_CAPACITY     = 100
+	CACHE_CANDIDATES_SIZE = 10
+)
 
-type CacheSettings struct {
-	MAX_SIZE   int
-	CACHE_SIZE int
+type CacheKV struct {
+	key   []byte
+	value []byte //Is a marshalled serverpb.CacheMeta
 }
 
-func (s *Server) LRUCache() {
-	// Iterate. Key only iteration for now.
-	// Check the number of keys in the local db
-	var numKeys = 0
-	keys := [][]byte{}
-	err := s.db.View(func(txn *badger.Txn) error {
-		opts := badger.DefaultIteratorOptions
-		opts.PrefetchValues = false
-		it := txn.NewIterator(opts)
-		defer it.Close()
+//Method that is called to cache an item
+func (s *Server) LRUCache(body *serverpb.CacheMeta) error {
 
-		for it.Rewind(); it.Valid(); it.Next() {
-			item := it.Item()
-			k := item.Key()
-			keys = append(keys, k)
-			numKeys += 1
-			fmt.Printf("key=%s\n", k)
+	lsm_size, _ := s.db.Size()
+	for lsm_size > MAX_BYTE_CAPACITY {
+		//perform cache eviction
+		err := s.cacheEvict()
+		if err != nil {
+			return err
 		}
-		return nil
-	})
+		lsm_size, _ = s.db.Size()
+	}
+
+	//call code to add to cache
+	err := s.AddToCache(body)
 
 	if err != nil {
 		fmt.Printf("error in cache function")
 	}
+}
 
-	// Do something about eviction.
-	// If number of keys is the maximum size,
-	// then an insertion of a document will overfill cache?
-	if numKeys == MAX_SIZE {
-		candidateKeys := [][]byte{}
-		// seed every time so indices aren't deterministic.
-		rand.Seed(time.Now().UnixNano())
+func (s *Server) AddToCache(body *serverpb.CacheMeta) error {
+	//GetRemoteFile
 
-		// Randomly select 10 keys from our db
-		for len(candidateKeys) < CACHE_SIZE {
-			randIndex := rand.Intn(100)
-			candidateKeys = append(candidateKeys, keys[randIndex])
-		}
-
-		// Fetch items from the database
-		s.db.View(func(txn *badger.Txn) error {
-			//TODO: inspect this and the iterator.
-			it := txn.NewIterator(badger.DefaultIteratorOptions)
-			defer it.Close()
-
-			for _, key := range candidateKeys {
-				for it.Seek(key); it.ValidForPrefix(key); it.Next() {
-					item := it.Item()
-					k := item.Key()
-					//TODO: fill an array with the key,value pairs and select one to evict.
-					v, err := item.Value()
-					if err != nil {
-						return err
-					}
-					fmt.Printf("key=%s, value=%s\n", k, v)
-				}
-			}
-
-			return nil
-		})
+	//TODO: item in cache has a timestamp
+	currTime := time.Now().UnixNano()
+	body.Marshal()
+	if err := s.db.Update(func(txn *badger.Txn) error {
+		return txn.Set([]byte("/client/%s"), docId)
+	}); err != nil {
+		return err
 	}
 
-	// TODO: what do we do about the new item wwe want to 'cache' locally.
+	// TODO: what do we do about the new item we want to 'cache' locally.
 	// Something with the file body, locally storing it
+
+	return nil
+}
+
+// When the DB is too big, start deleting things from /cache/ . Key points to /documents
+func (s *Server) cacheEvict() error {
+	// Iterate through keys, select 10 keys.
+	// Key only iteration for now.
+	// Check the number of keys in the local db.
+
+	cache := []CacheKV{}
+	var numKeys = 0
+	// Iterate over cache key prefix
+	s.db.View(func(txn *badger.Txn) error {
+		it := txn.NewIterator(badger.DefaultIteratorOptions)
+		defer it.Close()
+		prefix := []byte("/cache/")
+		for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+			item := it.Item()
+
+			k := item.Key()
+			v, err := item.Value()
+			if err != nil {
+				return err
+			}
+			cacheItem := CacheKV{k, v}
+
+			cache = append(cache, cacheItem)
+			numKeys += 1
+		}
+		return nil
+	})
+
+	// randomly selects 10 candidates from cache
+	rand.NewSource(time.Now().UnixNano())
+	var candidates []int
+	for len(candidates) < CACHE_CANDIDATES_SIZE {
+		candidates = append(candidates, rand.Intn(numKeys))
+	}
+
+	// iterate through elements, find the oldest one
+	for _, index := range candidates {
+		item := cache[index]
+		item.key.Unmarshal()
+		item.value.Unmarshal()
+	}
+
+	//TODO: Txn.Delete() to delete a key from the cache/documents
 
 }
